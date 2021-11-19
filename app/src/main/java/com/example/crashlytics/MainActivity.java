663b62dfddf1b095ccbd5bbe426550fd98f079da
Gotya Gosavi <prathameshgosavi015@gmail.com>
2021-11-19 17:15:21 +0530

    Final Commit

---
+++ app/src/main/java/com/example/crashlytics/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.crashlytics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_crash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
+        btn_crash=(Button) findViewById(R.id.btn_crash);
+
+        btn_crash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "test", Toast.LENGTH_SHORT).show();
+                throw new RuntimeException("Test Crash"); // Force a crash
+            }
+        });
+    }
+}
